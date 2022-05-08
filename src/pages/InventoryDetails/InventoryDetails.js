e455643edc522307bbea45b9eede2b1ba2da07b6
hasansharif819 <hs.sharif819@gmail.com>
2022-05-08 09:21:19 +0600

    form

--- src/pages/InventoryDetails/InventoryDetails.js
+++ src/pages/InventoryDetails/InventoryDetails.js
@@ -1,13 +1,24 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useParams } from 'react-router-dom';
+import auth from '../../firebase.init';
 import useInventoryDetails from '../../hooks/useInventoryDetails';
 
 const InventoryDetails = () => {
+    const [user] = useAuthState(auth);
     const {inventoriesId} = useParams();
     const [inventoryDetails] = useInventoryDetails(inventoriesId);
 
     return (
-        <div>
+        <div className='w-50 mx-auto'>
             <h2>Hello inventory: {inventoryDetails.name}</h2>
+            <form>
+                <input className='w-100 mb-2' type="text" name='name' placeholder='name' required />
+                <input className='w-100 mb-2' type="email" value={user.email} name='email' placeholder='email' readOnly required />
+                <input className='w-100 mb-2' type="text" value={inventoryDetails.name} name='inventoryName' placeholder='inventoryName' required />
+                <input className='w-100 mb-2' type="text" name='address' placeholder='address' readOnly required />
+                <input className='w-100 mb-2' type="text" name='phone' placeholder='phone' required />
+                <input type="submit" value="Delevered" />
+            </form>
         </div>
     );
 };
